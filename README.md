75aa7b5b35fab376917e27b9e97f23015a172820
Lorenzo Mancini <lmancini80@gmail.com>
2025-10-25 21:52:34 +0200

    Aggiunge e documenta utility per convertire sprite da formato Assembly Magellan a header MSXgl

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Chrono-runners By Associazione MSX Italia
+
+# Estrazione sprite da Magellan
+
+Esportare da Magellan gli sprite in formato "Assembly Data". Selezionare solo "Include Sprite Data", assicurarsi di scegliere l'inizio e la fine dell'esportazione in modo da prendere tutti i layer.
+
+Eseguire lo script Python mag2msx.py per convertire gli sprite nel formato MSXgl header.
+
+Se necessario, ridefinire al suo interno le variabili che contengono il numero di sprite, il numero di layer e la distanza in fotogrammi tra i layer.
+
+macOS:
+```console
+python3 Toolkit/mag2msx.py <file assembly>
+````
+
+Windows
+```console
+py Toolkit/mag2msx.py <file assembly>
+````
+
+Il programma produrrà automaticamente un file `data_sprt_layer.h` nella directory corrente; copiare quest'ultimo nella directory `content/sprite`.
---
+++ Toolkit/mag2msx.py
@@ -0,0 +1,53 @@
+import sys
+
+n_sprites = 15
+n_layers = 2
+layer_offset = 20
+
+sprites = []
+
+# Importa i dati del file .asm
+with open(sys.argv[1], "r") as fp:
+    sprite = []
+    for line in fp:
+
+        if line.startswith("SPR"):
+            if sprite:
+                sprites.append(sprite)
+            sprite = []
+
+        # Ignora l'header DATA
+        words = line.split(">")[1:]
+
+        # Prende solo i primi 4 caratteri
+        words = map(lambda w: w[0:4], words)
+
+        for w in words:
+
+            sprite.append(int(w[0:2], 16))
+            sprite.append(int(w[2:4], 16))
+
+# Scrive i dati nel file header in formato MSXgl
+with open("data_sprt_layer.h", "w") as fp:
+
+    fp.write("const unsigned char g_DataSprtLayer[] =\n")
+    fp.write("{\n")
+
+    for n_spr in range(n_sprites):
+        fp.write("// Frame %d\n" % n_spr)
+        for n_lay in range(n_layers):
+            fp.write("// Layer %d\n" % n_lay)
+            for i_val, val in enumerate(sprites[n_spr + n_lay * layer_offset]):
+
+                # Ogni 8 byte va a capo per leggibilità
+                if i_val != 0 and i_val % 8 == 0:
+                    fp.write("\n")
+
+                fp.write("0x"+f"{val:02X}")
+                fp.write(",")
+
+            fp.write("\n")
+
+        fp.write("\n")
+
+    fp.write("};\n")
