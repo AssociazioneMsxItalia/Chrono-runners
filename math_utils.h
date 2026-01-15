cba10ceebe2b7efdc1f2d9f00a74face9856a6d2
Lorenzo Mancini <lmancini80@gmail.com>
2026-01-15 23:18:27 +0100

    Funzione non utilizzata

--- math_utils.h
+++ math_utils.h
@@ -1,10 +1,5 @@
 #include "core.h"
 
-static inline i16 abs(i16 a)
-{
-	return (a > 0) ? a : -a;
-}
-
 // Divisione intera con segno tramite shift
 static inline i8 sdiv(i8 n, u8 k) {
 	i8 adj = (n >> k) & ((1 << k) - 1);
