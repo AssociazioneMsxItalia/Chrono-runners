5e118f5e9d211f9971cb1687acd5893b711f4e40
Antonello Carlomagno <developer@wau73.com>
2026-01-17 16:16:38 +0100

    Update 1_0_intro_04.h

--- content/levels/1_0_intro_04_lvl.h
+++ content/levels/1_0_intro_04_lvl.h
@@ -1,19 +1,18 @@
 extern unsigned char g_Level_intro[];
 
 struct Mine mines0[] = {
-	{5*8,  22*8, TRUE}, // pos_x pos_y
-	{24*8, 22*8, TRUE},
+	{22*8,  20*8, TRUE}, // pos_x pos_y
 };
 
 struct Level level_intro = {
-	1, 4,        // start_x start_y
-	1, 20,       // end_x end_y
-	27, 19,      // key_x key_y
+	1, 18,        // start_x start_y
+	29, 18,       // end_x end_y
+	14, 18,      // key_x key_y
 	0, 0,        // crystal_x crystal_y
-	1,           // num_platforms
-	plat1,       // platforms
-	2,           // num_mines
-	mines1,      // mines
+	0,           // num_platforms
+	NULL,       // platforms
+	1,           // num_mines
+	mines0,      // mines
 	0,           // num_enemies
 	NULL,        // enemies
 	g_Level_intro,   // layout
