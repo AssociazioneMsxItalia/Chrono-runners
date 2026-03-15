78f66ff8a67681cdb72e5faded92d130e5fcd5b3
Lorenzo Mancini <lmancini80@gmail.com>
2026-03-15 01:21:43 +0100

    Semplifica livello Easy Peasy che usa >100% di CPU

--- content/levels/map30_lvl.h
+++ content/levels/map30_lvl.h
@@ -12,39 +12,13 @@ struct Platform platforms_map30[] = {
 struct Mine mines_map30[] = {
 	{29*8, 23*8}, // pos_x pos_y
 	{29*8, 14*8}, // pos_x pos_y
-	{4*8, 17*8}, // pos_x pos_y
-	{1*8, 23*8}, // pos_x pos_y
-	{6*8, 17*8} // pos_x pos_y
+	{1*8, 23*8} // pos_x pos_y
 };
 
 struct Enemy enemies_map30[] = {
-  {20*8, 12*8,  // pos_x pos_y
-	        1,  // dir_x  TODO: set direction (-1 or 1)
-	17*8, 28*8,  // min_x max_x  TODO: adjust patrol range
-	        1,  // type (0-3)
-	        0,  // mDX (initialized to 0)
-	        0,  // stunned_timer (initialized to 0)
-			0,  // field_state
-			0,  // field_timer
-			0,  // field_x
-			0,  // field_y
-			0,  // field_mDX
-			0}, // field_dir
-  {15*8, 6*8,  // pos_x pos_y
-	        1,  // dir_x  TODO: set direction (-1 or 1)
-	12*8, 24*8,  // min_x max_x  TODO: adjust patrol range
-	        0,  // type (0-3)
-	        0,  // mDX (initialized to 0)
-	        0,  // stunned_timer (initialized to 0)
-			0,  // field_state
-			0,  // field_timer
-			0,  // field_x
-			0,  // field_y
-			0,  // field_mDX
-			0}, // field_dir
   {10*8, 15*8,  // pos_x pos_y
 	        1,  // dir_x  TODO: set direction (-1 or 1)
-	8*8, 24*8,  // min_x max_x  TODO: adjust patrol range
+	9*8, 24*8,  // min_x max_x  TODO: adjust patrol range
 	        2,  // type (0-3)
 	        0,  // mDX (initialized to 0)
 	        0,  // stunned_timer (initialized to 0)
@@ -79,7 +53,7 @@ struct Level level_map30 = {
 	mines_map30,        // mines
 	numberof(enemies_map30),  // num_enemies
 	enemies_map30,  // enemies
-	2,           // key_trigger_enemy (enemy 2 reveals key)
+	0,           // key_trigger_enemy (enemy 0 reveals key)
 	g_Screen30,   // layout
 	"EASY PEASY - LORENZO MORETTI",  // name
 };
